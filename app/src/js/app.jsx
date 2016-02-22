bf7da56bdd7102962aa188d35da2395a8f9a3e80
Jason Wharff <fishermanswharff@mac.com>
2016-02-22 07:45:44 -0500

    correct use of rendering the router

--- app/src/js/app.jsx
+++ app/src/js/app.jsx
@@ -5,12 +5,10 @@ import { Router, Route, Link, browserHistory } from 'react-router'
 import TodoApp from './components/todoApp';
 import TodoLists from './components/todoLists'
 
-ReactDOM.render(<TodoApp/>, document.getElementById('todo-list'));
-
-render((
+ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={TodoApp}>
     </Route>
     <Route path="/lists/:listId" component={TodoLists}/>
   </Router>
-), document.body);
+), document.getElementById('todo-list'));
