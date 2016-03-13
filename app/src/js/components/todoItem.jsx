abd91fb3900c01249869e239b69f070d48cccbe4
Jason Wharff <fishermanswharff@mac.com>
2016-03-13 16:21:15 -0400

    listing # of todos on a list in the menu

--- app/src/js/components/todoItem.jsx
+++ app/src/js/components/todoItem.jsx
@@ -3,12 +3,33 @@ import { Link } from 'react-router'
 
 export default class TodoItem extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.getChildren = this.getChildren.bind(this);
+
+    this.state = {
+      numTasks: 0
+    }
+  }
+
+  getChildren(){
+    var todoItemsRef = new Firebase(`https://jwtodoapp.firebaseio.com/tasks/${this.props.firebaseKey}`);
+    todoItemsRef.once('value', (dataSnapshot) => {
+      this.setState({numTasks: dataSnapshot.numChildren()});
+    });
+  }
+
+  componentDidMount(){
+    this.getChildren();
+  }
+
   render() {
     return (
       <li key={this.props.data.name}>
         <Link activeStyle={{ color: 'red' }} to={`/lists/${this.props.firebaseKey}`}>
           {this.props.data.name}
         </Link>
+        {this.state.numTasks}
       </li>
     )
   }
--- app/src/js/components/todoLists.jsx
+++ app/src/js/components/todoLists.jsx
@@ -19,9 +19,7 @@ export default class TodoLists extends React.Component {
       <section className='todo-lists-container'>
         <header></header>
         <ul className='todo-lists'>
-          {this.props.items.map((item,idx) => {
-            return <TodoItem key={item.key} data={item.list} firebaseKey={item.key} />
-          })}
+          {this.props.items.map(this.createItem)}
         </ul>
         <div className='todo-lists-children'>
           {this.props.children}
